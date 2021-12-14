22d705ac300c6aaac140eec5a30680c9161d0e8d
vectorvactor <vgrandec01@educantabria.es>
2021-12-14 11:56:45 +0100

    Creado nuevo ComponenteLista

--- src/components/ComponenteEj.jsx
+++ src/components/ComponenteEj.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import ComponenteLista from './ComponenteLista';
 export function ComponenteEj(props) {
   return (
     <div>
       {props.titulo} - {props.icono}
       <ul>
-        <ComponenteLista texto="Elemento 1" >
+        <ComponenteLista texto="Elemento 1" />
         <li>Elemento 2</li>
         <li>Elemento 3</li>
       </ul>
---
+++ src/components/ComponenteLista.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ComponenteLista(props) {
+  return <li>{props.texto}</li>;
+}
