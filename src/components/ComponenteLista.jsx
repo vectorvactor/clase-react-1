146286aeb131d540a6f56edde7e95a626b442f58
vectorvactor <vgrandec01@educantabria.es>
2021-12-22 14:06:08 +0100

    Añadido estado a ComponenteLista (función)

--- src/components/ComponenteLista.jsx
+++ src/components/ComponenteLista.jsx
@@ -3,11 +3,6 @@ import React, { useState } from 'react';
 import './componente-lista.css';
 
 export default function ComponenteLista(props) {
-  let claseLista = '';
-
-  const [done, setDone] = useState(props.done);
-
-  setElementClass();
   function setElementClass() {
     claseLista = props.prioridad;
     if (done) {
@@ -21,6 +16,9 @@ export default function ComponenteLista(props) {
     setDone(!done);
   }
 
+  let claseLista = '';
+  const [done, setDone] = useState(props.done);
+  setElementClass();
   return (
     <li className={claseLista}>
       <input type="checkbox" defaultChecked={done} onChange={changeStatus} />
