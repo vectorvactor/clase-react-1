8ff8f34983aa8f9b320333369da3570f4385883f
vectorvactor <vgrandec01@educantabria.es>
2022-01-13 11:02:32 +0100

    Estilos en menú (roto)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,30 +7,34 @@ import './header.css';
 class Header extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      clicked: false,
+    };
+  }
+
+  changeClicked() {
+    this.setState({ clicked: !this.state.clicked });
   }
 
   render() {
     return (
-      <nav>
-        <h1>Mi aplicación</h1>
-        {/*
-        <ul>{this.createMenuLinks()}</ul>
-        <ul>
-          Forma 2: map (función) 
-          {MenuItems.map(function (item) {
-            return (
-              <li>
-                <Link to={item.path}>{item.title}</Link>
-              </li>
-            );
-          })}
-        </ul>
-        */}
-        <ul>
-          {/* Forma 3: map (función flecha) */}
+      <nav className="header-navbar">
+        <div className="navbar-logo">
+          <img
+            className="logo-image"
+            src="https://eu01.edcwb.com/buscador/img/centros/logogrande/7348-a9c730d6b2b644f5b9910364ba6af277.jpg"
+          />
+          <i className="fas fa-user-graduate" />
+        </div>
+        <div className="menu-icon" onClick={this.changeClicked.bind(this)}>
+          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul
+          className={this.state.clicked ? 'navbar-menu active' : 'navbar-menu'}
+        >
           {MenuItems.map((item) => {
             return (
-              <li key={item.id}>
+              <li key={item.id} className="navbar-link">
                 <Link to={item.path}>{item.title}</Link>
               </li>
             );
@@ -38,21 +42,6 @@ class Header extends React.Component {
         </ul>
       </nav>
     );
-  }
-
-  /**
-   * Forma menú nº 1: bucle for
-   */
-  createMenuLinks() {
-    const listComponents = [];
-    for (let i = 0; i < MenuItems.length; i++) {
-      listComponents.push(
-        <li>
-          <Link to={MenuItems[i].path}>{MenuItems[i].title}</Link>
-        </li>
-      );
-    }
-    return listComponents;
   }
 }
 
