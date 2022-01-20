fa9845e7c4047fa289d84a563979a8d74e92d43e
vectorvactor <vgrandec01@educantabria.es>
2022-01-20 09:05:57 +0100

    Añadida información de coches que faltaba

--- src/data/DatosCoches.js
+++ src/data/DatosCoches.js
@@ -5,19 +5,24 @@ export const TitulosTablaCoches = {
   field3: 'Color',
 };
 
-// TODO: rellenar atributos que faltan para todos los coches
 export const DatosTablaCoches = [
   {
     matricula: '0000 LOL',
     marca: 'Peugeot',
     modelo: 'Partner',
     color: 'rojo',
+    imagen:
+      'https://img.remediosdigitales.com/489069/peugeot-partner-outdoor-1/1366_2000.jpg',
+    descripción: 'Una furgoneta chachi para ti, perroflauta',
   },
   {
     matricula: '1111 YES',
     marca: 'Hyundai',
     modelo: 'ix35',
     color: 'verde',
+    imagen:
+      'https://www.manualesdetodo.net/wp-content/uploads/2020/11/2012-ix35-hyundai-u-1200x720.jpg',
+    descripción: 'Un esiuvi, parece un 4x4 pero no lo es',
   },
   {
     matricula: '2222 WOW',
@@ -26,12 +31,15 @@ export const DatosTablaCoches = [
     color: 'blanco',
     imagen:
       'https://www.mercadoracing.org/imagenes-anuncios/26/419711/mitsubishi-evo-vii-rs.jpg',
-    descripción: 'Un coche genial',
+    descripción: 'Un coche genial para tus carreras ilegales',
   },
   {
     matricula: '3333 FFF',
     marca: 'Subaru',
     modelo: 'Impreza',
     color: 'azul',
+    imagen:
+      'https://periodismodelmotor.com/wp-content/uploads/2019/06/Subaru-Impreza-22B-STI.jpg',
+    descripción: 'Este esta muy bien también para reisin',
   },
 ];
